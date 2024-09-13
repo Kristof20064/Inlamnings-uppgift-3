efe63dfadbbb3de38b27072e013e86cf7968ecde
krbu06001 <krbu06001@E-PW05CGQY>
2024-09-13 08:51:19 +0200

    Add project files.

---
+++ Inlamnings uppgift 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inlamnings_uppgift_3
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \" \"\\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
